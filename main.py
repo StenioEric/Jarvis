1d20a7d1c62a0b042987c318e4a7b6321e254829
Stenio Eric <stenioeric@gmail.com>
2024-01-07 16:00:00 -0300

    Adicionado reconhecimento de voz para Português

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import speech_recognition as sr
 
+
 #Cria um reconhecedor 
 r = sr.Recognizer()
 
 #Abrir o microfone para captura
 with sr.Microphone() as source:
-    audio = r.listen(source) #Define microfone como fonte de áudio
 
-    print(r.recognize_google(audio))
+    while True:
+
+        audio = r.listen(source) #Define microfone como fonte de áudio
+
+        print(r.recognize_google(audio, language ='pt'))
+
